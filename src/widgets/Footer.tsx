55b253650657ffa54016d6f171c588bd029808af
Eduard Stromenko <estromenko@mail.ru>
2022-06-11 21:05:35 +0300

    add footer

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Container } from '../shared/ui';
+import { Box } from '@mui/material';
+
+import homeImage from '../assets/home.png';
+import tasksImage from '../assets/tasks.png';
+import giftImage from '../assets/gift.png';
+import ratingImage from '../assets/rating.png';
+import settingsImage from '../assets/settings.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <header>
+    <footer>
       <Container>
-        <p>data</p>
+        <Box bgcolor="primary.main" display="flex" flexDirection="row" justifyContent="space-around" padding="10px">
+          <Link to="/home">
+            <img src={homeImage} alt="home" />
+          </Link>
+          <Link to="/tasks">
+            <img src={tasksImage} alt="tasks" />
+          </Link>
+          <Link to="/gift">
+            <img src={giftImage} alt="gift" />
+          </Link>
+          <Link to="/rating">
+            <img src={ratingImage} alt="rating" />
+          </Link>
+          <Link to="/settings">
+            <img src={settingsImage} alt="settings" />
+          </Link>
+        </Box>
       </Container>
-    </header>
+    </footer>
   );
 };
 
